e0a4c24e1bc77050cd56ec44d5068d0940321d08
Manoel Campos <manoelcampos@gmail.com>
2017-10-23 22:29:29 -0300

    Alteradas imagens estáticas do BlueJ por gifs

---
+++ video2gif.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+
+if [ $# -eq 0 ]; then
+   echo "Usage: $0 video-file-name [output-file-name]"
+   exit -1
+fi
+
+IN="$1"
+OUT="${IN%.*}.gif"
+
+if [ $# -gt 1 ]; then
+   OUT="$2"
+fi
+
+echo "Generating file $OUT"
+
+#-s 600x400 
+#--delay=3
+# -r 3
+ffmpeg -i "$IN" -pix_fmt rgb24 -r 3 -f gif - | gifsicle --optimize=3  > "$OUT"
