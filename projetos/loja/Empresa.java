af9dc823912444615e52069f6f89ef6df9917fd7
Manoel Campos <manoelcampos@gmail.com>
2017-11-04 10:29:58 -0300

    Atualizado capítulo 8 e pequenas correções

--- projetos/loja/Empresa.java
+++ projetos/loja/Empresa.java
@@ -1,33 +1,9 @@
 
-/**
- * Escreva a descrição da classe Empresa aqui.
- * 
- * @author (seu nome) 
- * @version (número de versão ou data)
- */
 public class Empresa
 {
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
-
-    /**
-     * COnstrutor para objetos da classe Empresa
-     */
-    public Empresa()
-    {
-        // inicializa variáveis de instância
-        x = 0;
-    }
-
-    /**
-     * Exemplo de método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   exemplo de um parâmetro de método
-     * @return     a soma de x com y 
-     */
-    public int sampleMethod(int y)
-    {
-        // ponha seu código aqui
-        return x + y;
-    }
+    String endereco;
+    String cidade;
+    String telefone;
+    String email;
+    boolean matriz;
 }
