c8294a8e41f1e3ce3dd2ddd29449751f94ce09a8
Manoel Campos <manoelcampos@gmail.com>
2017-11-09 16:03:14 -0300

    Alterado video para definir tamanho em percentual

--- projetos/loja/Empresa.java
+++ projetos/loja/Empresa.java
@@ -2,24 +2,37 @@
 public class Empresa
 {
     private int codigo;
-    String endereco;
-    String cidade;
+    private String endereco;
+    private String cidade;
     String telefone;
     String email;
     String cnpj;
-    boolean matriz;
     Funcionario gerente;
+    boolean matriz;
+    
+    Empresa(){
+        this.cidade = "Palmas";
+    }
     
     Empresa(Funcionario gerente){
         setGerente(gerente);
+        gerente.setNome("José");
+    }
+    
+    Funcionario getGerente(){
+        return gerente;
     }
     
     void setGerente(Funcionario gerente){
         this.gerente = gerente;
     }
     
-    Funcionario getGerente(){
-        return gerente;
+    boolean isMatriz(){
+        return matriz;
+    }
+    
+    void setMatriz(boolean matriz){
+        this.matriz = matriz;
     }
     
     String getEndereco(){
@@ -28,18 +41,5 @@ public class Empresa
     
     void setEndereco(String endereco){
         this.endereco = endereco;
-    }
-    
-    boolean isMatriz(){
-        return matriz;
-    }
-    
-    boolean isFilial(){
-        //returna true se a empresa não for matriz
-        return !matriz;
-    }
-    
-    void setMatriz(boolean matriz){
-        this.matriz = matriz;
     }
 }
--- projetos/loja/Funcionario.java
+++ projetos/loja/Funcionario.java
@@ -1,10 +1,25 @@
 public class Funcionario
 {
-    private String nome;
+    private String nome; 
+    String cidade;
     String matricula;
     String cpf;
     String cargo;
-    String cidade;
+
+    Funcionario(){
+        this.cidade = "Palmas";
+    }
+
+    Funcionario(String nome, String cidade){
+       setCidade(cidade);
+       setNome(nome);
+       this.cpf = "00000000000";
+       this.matricula = "0";
+    }    
+    
+    void setCidade(String cidade){
+        this.cidade = cidade;
+    }
     
     void setNome(String nome){
         this.nome = nome;
